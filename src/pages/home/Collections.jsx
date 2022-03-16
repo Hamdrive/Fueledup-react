7de03ac2ec6be9610a6d7d0e5c7916bb056420f1
Hamdrive <hamza.husein017@gmail.com>
2022-03-16 06:47:57 +0530

    [update] className attribute

--- src/pages/home/Collections.jsx
+++ src/pages/home/Collections.jsx
@@ -4,21 +4,21 @@ import styles from "./Home.module.css";
 
 export default function Collections() {
   return (
-    <section class="recent-products py-md">
+    <section className="recent-products py-md">
       <h3>
         Browse various
         <span className={styles.imp__word}> collections </span>
         ...
       </h3>
-      <div class="collections flex-wrap flex-row gap-1 mx-auto">
+      <div className="collections flex-wrap flex-row gap-1 mx-auto">
         <div className={`${styles.category} flex-center flex-grow-1`}>
-          <img class="w-100 blur" src={Collection1} alt="cleareance" />
+          <img className="w-100 blur" src={Collection1} alt="cleareance" />
           <div className={`pos-ab ${styles.collection__cover__text} px-sm txt-lg txt-bold`}>
             Clearance SALE
           </div>
         </div>
         <div className={`${styles.category} flex-center flex-grow-1`}>
-          <img class="w-100 blur" src={Collection2} alt="2020 season apparel" />
+          <img className="w-100 blur" src={Collection2} alt="2020 season apparel" />
           <div className={`pos-ab ${styles.collection__cover__text} px-sm txt-lg txt-bold`}>
             2022 Season Apparel
           </div>
