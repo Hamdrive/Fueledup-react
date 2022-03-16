48eda649e9baf8f124fbb3ef9907ee12631a23c7
Hamdrive <hamza.husein017@gmail.com>
2022-03-16 06:45:25 +0530

    [refactor] import and components restructure

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,42 +1,20 @@
-// import "./home.css";
-import Banner from "../../assets/hero-image.webp";
-import { Navbar } from "../../components/Navbar";
+import { Navbar, Footer } from "../../components/index";
 import Categories from "./Categories";
 import Collections from "./Collections";
-import { Footer } from "../../components/Footer";
-import { Link } from "react-router-dom";
-import styles from "./Home.module.css"
-import "../../styles/utils.css"
+import HeroSection from "./HeroSection";
+import styles from "./Home.module.css";
+import "../../styles/utils.css";
 
-export default function Home() {
+export function Home() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Navbar />
-      <div class="main">
+      <div className="main">
         <main>
           <section className={styles.hero__section}>
-            <div className={styles.hero}>
-              <img src={Banner} alt="hero-image" />
-            </div>
-            <div className={`${styles.hero__overlay} pos-ab flex-column`}>
-              <h1>
-                For the love of
-                <em> racing </em>
-              </h1>
-              <h2>
-                Driven by fans,
-                <em> FueledUp </em>
-                by fans.
-              </h2>
-              <Link to="/products">
-                <button class="btn btn-product btn-cta btn-md flex-center txt-bold txt-reg w-60 mx-auto mt-1 mb-1 txt-wrap">
-                  <i class="fas fa-flag-checkered txt-md rotate-icon"></i>
-                  Check out Products
-                </button>
-              </Link>
-            </div>
+            <HeroSection />
           </section>
-          <section class="my-2 px-lg">
+          <section className="my-2 px-lg">
             <Categories />
             <Collections />
           </section>
