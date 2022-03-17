5b557498ee217493bff14d3b6d7b92203631d1e4
Hamdrive <hamza.husein017@gmail.com>
2022-03-17 07:16:37 +0530

    [feat] create dynamic content products page

--- src/pages/products/ProductCards.jsx
+++ src/pages/products/ProductCards.jsx
@@ -1,44 +1,80 @@
 import styles from "./Products.module.css";
 import Hat from "../../assets/card-img1.png";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function ProductCards() {
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    try {
+      const res = await axios.get("/api/products");
+      console.log(res.data.products);
+      setProducts(res.data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <section
       className={`grid ${styles.grid__cols__auto} ${styles.grid__col__1} ${styles.grid__cols__2} gap-2 my-3 px-md`}>
-      <div className={`pos-rel card ${styles.card} mx-auto`}>
-        <div className="pos-ab card-badge card-badge-left top-left-pos">
-          10% off
-        </div>
-        <div
-          className={`pos-ab ${styles.top__right__pos} flex-center ${styles.border__circle} ${styles.wish__heart__btn}`}>
-          <i className="far fa-heart"></i>
-        </div>
-        <div className={`${styles.card__img}`}>
-          <img src={Hat} alt="vertical card" />
-        </div>
-        <div className="card-title flex-col flex-grow-1">
-          <p className="txt-md txt-bold">MV33 Brimmed Cap</p>
-          <p className="txt-reg txt-semibold">Headwear</p>
-          <div className="flex-row align-center mt-1">
-            <div className="dis-flex flex-wrap align-center">
-              <p className="txt-md txt-semibold mr-sm">₹3,099/-</p>
-              <s className="strike-color txt-semibold">₹3,400/-</s>
+      {products.map((product) => (
+        <div className={`pos-rel card ${styles.card} mx-auto`}>
+          {!product.inStock && (
+            <div
+              className={`brief-overlay ${styles.brief__overlay} pos-ab top-left-pos`}>
+              <p
+                className={`overlay-text ${styles.overlay__text} pos-ab txt-black mx-auto txt-center`}>
+                Out&nbsp;Of Stock
+              </p>
             </div>
-            <p className="flex-basis-20 txt-sm txt-bold ml-auto rating">
-              <i className="fas fa-sm fa-star"></i>
-              4.9
+          )}
+          <div className="pos-ab card-badge card-badge-left top-left-pos">
+            {product.productDiscount}% off
+          </div>
+          <div
+            className={`pos-ab ${styles.top__right__pos} flex-center ${styles.border__circle} ${styles.wish__heart__btn}`}>
+            <i className="far fa-heart"></i>
+          </div>
+          <div className={`${styles.card__img}`}>
+            <img src={product.productImage} alt="vertical card" />
+          </div>
+          <div className="card-title flex-col flex-grow-1">
+            <p className="txt-md txt-bold">{product.title}</p>
+            <p className="txt-reg txt-semibold">
+              {product.author} - {product.categoryName}
             </p>
+            <div className="flex-row align-center mt-1">
+              <div className="dis-flex flex-wrap align-center">
+                <p className="txt-md txt-semibold mr-sm">
+                  ₹{" "}
+                  {Number(product.price) *
+                    (
+                      1 - Number(`0.${Number(product.productDiscount)}`)
+                    ).toFixed(0)}
+                </p>
+                <s className="strike-color txt-semibold">₹ {product.price}</s>
+              </div>
+              <p className="flex-basis-20 txt-sm txt-bold ml-auto rating">
+                <i className="fas fa-sm fa-star"></i>
+                {product.productRating}
+              </p>
+            </div>
+          </div>
+          <hr />
+          <div className="card-footer flex-around flex-grow-1">
+            <button className="btn btn-atc btn-cta btn-md txt-bold txt-reg w-100">
+              <i className="fas fa-shopping-cart"></i>
+              Add to cart
+            </button>
           </div>
         </div>
-        <hr />
-        <div className="card-footer flex-around flex-grow-1">
-          <button className="btn btn-atc btn-cta btn-md txt-bold txt-reg w-100">
-            <i className="fas fa-shopping-cart"></i>
-            Add to cart
-          </button>
-        </div>
-      </div>
-      <div className={`pos-rel card ${styles.card} mx-auto`}>
+      ))}
+      {/* <div className={`pos-rel card ${styles.card} mx-auto`}>
         <div className="pos-ab card-badge card-badge-left top-left-pos">
           10% off
         </div>
@@ -143,7 +179,7 @@ export default function ProductCards() {
             Add to cart
           </button>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
