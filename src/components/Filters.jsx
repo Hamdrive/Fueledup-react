6d225aebe77b39d05b0853ca9ca41aeedb26895d
Hamdrive <hamza.husein017@gmail.com>
2022-03-18 15:32:39 +0530

    [feat] context in filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,12 @@
+import { useProducts } from "../utils/product-context";
+
 export function Filters() {
+  const { state, dispatch } = useProducts();
   return (
     <aside className="min-vh-85 filters top-left-pos pl-2 border-r-2 mob-aside">
       <header className="pos-st flex-between mb-1 px-md mob-header">
         <h4 className="txt-black">Filters</h4>
-        <button className="btn btn-sm btn-sec px-md">Clear</button>
+        <button onClick={()=>dispatch({type: "CLEAR"})} className="btn btn-sm btn-sec px-md">Clear</button>
       </header>
       <section className="filter-section mob-hide">
         <section className="mt-2 px-md">
@@ -14,8 +17,12 @@ export function Filters() {
               min="0"
               max="10000"
               step="2500"
+              value={state.priceRange}
               list="tickmarks"
               className="slider"
+              onChange={(e) =>
+                dispatch({ type: "PRICE", payload: e.target.value })
+              }
             />
             <datalist id="tickmarks" className="flex-between txt-sm">
               <option value="0" className="txt-semibold" label="0"></option>
@@ -33,197 +40,299 @@ export function Filters() {
           <h4 className="txt-black">Category</h4>
 
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="masks"
-            />
-            <label htmlFor="masks" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "CATEGORY", payload: e.target.id })
+              }
+              htmlFor="masks"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="category"
+                id="Masks"
+                value="Masks"
+              />
               Masks
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="clothing"
-            />
-            <label htmlFor="clothing" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "CATEGORY", payload: e.target.id })
+              }
+              htmlFor="clothing"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="category"
+                id="Clothing"
+                value="Clothing"
+              />
               Clothes
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="bags"
-            />
-            <label htmlFor="bags" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "CATEGORY", payload: e.target.id })
+              }
+              htmlFor="bags"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="category"
+                id="Bags"
+                value="Bags"
+              />
               Bags
             </label>
           </article>
 
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="headwear"
-            />
-            <label htmlFor="headwear" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "CATEGORY", payload: e.target.id })
+              }
+              htmlFor="headwear"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="category"
+                id="Headwear"
+                value="Headwear"
+              />
               Headwear
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="models"
-            />
-            <label htmlFor="models" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "CATEGORY", payload: e.target.id })
+              }
+              htmlFor="models"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="category"
+                id="Models"
+                value="Models"
+              />
               Models
             </label>
           </article>
         </section>
-        {/* <section className="mt-2 px-md">
+        <section className="mt-2 px-md">
           <h4 className="txt-black">Brands</h4>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="headwear"
-            />
-            <label htmlFor="headwear" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "BRAND", payload: e.target.id })
+              }
+              htmlFor="headwear"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="brand"
+                id="Alpeene"
+                value="Alpeene"
+              />
               Alpeene
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="masks"
-            />
-            <label htmlFor="masks" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "BRAND", payload: e.target.id })
+              }
+              htmlFor="masks"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="brand"
+                id="Alpha-Ball"
+                value="Alpha-Ball"
+              />
               Alpha-Ball
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="clothing"
-            />
-            <label htmlFor="clothing" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "BRAND", payload: e.target.id })
+              }
+              htmlFor="clothing"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="brand"
+                id="Anton Marten"
+                value="Anton Marten"
+              />
               Anton Marten
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="bags"
-            />
-            <label htmlFor="bags" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "BRAND", payload: e.target.id })
+              }
+              htmlFor="bags"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="brand"
+                id="Bclaren"
+                value="Bclaren"
+              />
               Bclaren
             </label>
           </article>
 
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="models"
-            />
-            <label htmlFor="models" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "BRAND", payload: e.target.id })
+              }
+              htmlFor="models"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="brand"
+                id="Merdeces"
+                value="Merdeces"
+              />
               Merdeces
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="models"
-            />
-            <label htmlFor="models" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "BRAND", payload: e.target.id })
+              }
+              htmlFor="models"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="brand"
+                id="Purrari"
+                value="Purrari"
+              />
               Purrari
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="models"
-            />
-            <label htmlFor="models" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "BRAND", payload: e.target.id })
+              }
+              htmlFor="models"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="brand"
+                id="Red-Ball"
+                value="Red-Ball"
+              />
               Red-Ball
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="checkbox"
-              className="checkbox mr-3"
-              name="category"
-              id="models"
-            />
-            <label htmlFor="models" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "BRAND", payload: e.target.id })
+              }
+              htmlFor="models"
+              className="txt-reg txt-semibold">
+              <input
+                type="checkbox"
+                className="checkbox mr-3"
+                name="brand"
+                id="Welleams"
+                value="Welleams"
+              />
               Welleams
             </label>
           </article>
-        </section> */}
+        </section>
         <section className="mt-2 px-md">
           <h4 className="txt-black">Ratings</h4>
           <article className="mx-1 py-sm">
-            <input
-              type="radio"
-              className="radiobutton mr-3"
-              name="review"
-              id="review-4"
-            />
-            <label htmlFor="review-4" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "RATING", payload: e.target.id })
+              }
+              htmlFor="review-4"
+              className="txt-reg txt-semibold">
+              <input
+                type="radio"
+                className="radiobutton mr-3"
+                name="review"
+                id="4"
+                checked={state.ratings === 4}
+              />
               4 Stars & above
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="radio"
-              className="radiobutton mr-3"
-              name="review"
-              id="review-3"
-            />
-            <label htmlFor="review-3" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "RATING", payload: e.target.id })
+              }
+              htmlFor="review-3"
+              className="txt-reg txt-semibold">
+              <input
+                type="radio"
+                className="radiobutton mr-3"
+                name="review"
+                id="3"
+                checked={state.ratings === 3}
+              />
               3 Stars & above
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="radio"
-              className="radiobutton mr-3"
-              name="review"
-              id="review-2"
-            />
-            <label htmlFor="review-2" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "RATING", payload: e.target.id })
+              }
+              htmlFor="review-2"
+              className="txt-reg txt-semibold">
+              <input
+                type="radio"
+                className="radiobutton mr-3"
+                name="review"
+                id="2"
+                checked={state.ratings === 2}
+              />
               2 Stars & above
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="radio"
-              className="radiobutton mr-3"
-              name="review"
-              id="review-1"
-            />
-            <label htmlFor="review-1" className="txt-reg txt-semibold">
+            <label
+              onChange={(e) =>
+                dispatch({ type: "RATING", payload: e.target.id })
+              }
+              htmlFor="review-1"
+              className="txt-reg txt-semibold">
+              <input
+                type="radio"
+                className="radiobutton mr-3"
+                name="review"
+                id="1"
+                checked={state.ratings === 1}
+              />
               1 Star & above
             </label>
           </article>
@@ -231,24 +340,32 @@ export function Filters() {
         <section className="mt-2 px-md">
           <h4 className="txt-black">Sort by</h4>
           <article className="mx-1 py-sm">
-            <input
-              type="radio"
-              className="radiobutton mr-3"
-              name="sort"
-              id="sort-high"
-            />
-            <label htmlFor="sort-high" className="txt-reg txt-semibold">
+            <label
+              onChange={() => dispatch({ type: "HIGH", payload: "HIGH" })}
+              htmlFor="sort-high"
+              className="txt-reg txt-semibold">
+              <input
+                type="radio"
+                className="radiobutton mr-3"
+                name="sort"
+                id="sort-high"
+                checked={state.priceSort === "HIGH"}
+              />
               High to Low
             </label>
           </article>
           <article className="mx-1 py-sm">
-            <input
-              type="radio"
-              className="radiobutton mr-3"
-              name="sort"
-              id="sort-low"
-            />
-            <label htmlFor="sort-low" className="txt-reg txt-semibold">
+            <label
+              onChange={() => dispatch({ type: "LOW", payload: "LOW" })}
+              htmlFor="sort-low"
+              className="txt-reg txt-semibold">
+              <input
+                type="radio"
+                className="radiobutton mr-3"
+                name="sort"
+                id="sort-low"
+                checked={state.priceSort === "LOW"}
+              />
               Low to High
             </label>
           </article>
