e4e30cbca500309d3f4737471b1eb71acf48ed42
Hamdrive <hamza.husein017@gmail.com>
2022-03-18 13:09:22 +0530

    [feat] switch statement for reducer hook

--- src/utils/applyFilters.js
+++ src/utils/applyFilters.js
@@ -1,14 +1,36 @@
-
-
-import React from 'react'
+import { filterSpecification } from "./filterSpecification";
 
 export function applyFilters(state, action) {
-    switch (action.type) {
-        case value:
-            
-            return;
-    
-        default:
-            return state
-    }
+  function updateCategories(state, category) {
+    return state["categories"].filter(
+      (existingCategory) => existingCategory !== category
+    );
+  }
+
+  switch (action.type) {
+    case "LOW":
+      return { ...state, priceSort: action.payload };
+    case "HIGH":
+      return { ...state, priceSort: action.payload };
+    case "INSTOCK":
+      return { ...state, inStock: !state.inStock };
+    case "FASTDELIVERY":
+      return { ...state, fastDelivery: !state.fastDelivery };
+    case "RATING":
+      return { ...state, ratings: action.payload };
+    case "PRICE":
+      return { ...state, priceRange: action.payload };
+    case "CATEGORY":
+      return {
+        ...state,
+        categories: state["categories"].includes(action.payload)
+          ? updateCategories(state, action.payload)
+          : state["categories"].concat(action.payload),
+      };
+    case "CLEAR":
+      return { ...filterSpecification };
+
+    default:
+      return state;
+  }
 }
