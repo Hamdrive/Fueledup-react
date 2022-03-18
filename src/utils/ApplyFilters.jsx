2e23c61174204e7de9f020053417d7dcabbfbe9d
Hamdrive <hamza.husein017@gmail.com>
2022-03-18 11:10:49 +0530

    [feat] intial filters switch statement

---
+++ src/utils/ApplyFilters.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+export function ApplyFilters(state, action) {
+    switch (action.type) {
+        case value:
+            
+            return;
+    
+        default:
+            return state
+    }
+}
