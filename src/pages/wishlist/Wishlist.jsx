ed9805d3da339bb2e03a35b25f68b9d143f1c8de
Hamdrive <hamza.husein017@gmail.com>
2022-03-18 17:52:31 +0530

    [feat] Wishlist page

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Footer, Navbar } from '../../components'
+import React from "react";
+import { Footer, Navbar } from "../../components";
+import { WishlistCards } from "./WishlistCards";
 
 export function Wishlist() {
   return (
     <>
       <Navbar />
+      <main className="max-width-1200 mx-auto">
+        <WishlistCards />
+      </main>
       <Footer />
     </>
-  )
+  );
 }
