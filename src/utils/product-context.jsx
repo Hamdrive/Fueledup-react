bc9b26719aa9c3722f43bb8e007be568b9fbefac
Hamdrive <hamza.husein017@gmail.com>
2022-03-18 15:34:36 +0530

    [refactor][fix] IIFE products and brand reducer

--- src/utils/product-context.jsx
+++ src/utils/product-context.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import {
   createContext,
   useContext,
@@ -6,7 +7,15 @@ import {
   useEffect,
 } from "react";
 import { applyFilters } from "./applyFilters";
-import { fastDelivery, inStock, priceRange, sortByPrice, userCategory, userRating } from "./filterFunctions";
+import {
+  fastDelivery,
+  inStock,
+  priceRange,
+  sortByPrice,
+  userBrand,
+  userCategory,
+  userRating,
+} from "./filterFunctions";
 import { filterSpecification } from "./filterSpecification";
 import { getFinalProducts } from "./getFinalProducts";
 
@@ -18,19 +27,27 @@ const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const [state, dispatch] = useReducer(applyFilters, filterSpecification);
 
-  const finalProducts = getFinalProducts(sortByPrice, inStock, fastDelivery, userRating, userCategory, priceRange)(state, [...products]);
+  console.log(state)
+
+  const finalProducts = getFinalProducts(
+    sortByPrice,
+    inStock,
+    fastDelivery,
+    userRating,
+    userCategory,
+    userBrand,
+    priceRange
+  )(state, [...products]);
 
   useEffect(() => {
-    {
-      async () => {
-        try {
-          const res = await axios.get("/api/products");
-          setProducts(res.data.products);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-    }
+    (async () => {
+      try {
+        const res = await axios.get("/api/products");
+        setProducts(res.data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
   }, []);
 
   return (
