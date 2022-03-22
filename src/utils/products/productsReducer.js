6a8aac0e1f7eff8282e311d8e7d3d840af5faf64
Hamdrive <hamza.husein017@gmail.com>
2022-03-22 23:21:19 +0530

    [fix] remove wishlist filter function

--- src/utils/products/productsReducer.js
+++ src/utils/products/productsReducer.js
@@ -1,9 +1,15 @@
 export function productsReducer(state, action) {
+  console.log(state, action);
   switch (action.type) {
     case "ADD_TO_WISHLIST":
       return { ...state, wishlist: [...state.wishlist, action.payload] };
     case "REMOVE_FROM_WISHLIST":
-      return state["wishlist"].filter((item) => item.id !== action.payload._id);
+      return {
+        ...state,
+        wishlist: state["wishlist"].filter(
+          (item) => item._id !== action.payload._id
+        ),
+      };
     default:
       return state;
   }
