e7dbfa9691cf8140d735cc553b06f84e02d4825c
Hamdrive <hamza.husein017@gmail.com>
2022-03-22 18:44:53 +0530

    [chore] remove index css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ProductProvider } from "./utils/product-context";
+import { ProductProvider } from "./context/product-context";
 
 // Call make Server
 makeServer();
