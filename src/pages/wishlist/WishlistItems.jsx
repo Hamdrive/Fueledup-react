eda1d126d457f74046d6c30a81f55f4fd9ef3d79
Hamdrive <hamza.husein017@gmail.com>
2022-03-22 23:21:49 +0530

    [feat] show wishlist cards based on context

--- src/pages/wishlist/WishlistItems.jsx
+++ src/pages/wishlist/WishlistItems.jsx
@@ -1,178 +1,22 @@
 import React from "react";
 import styles from "./Wishlist.module.css";
 import Hat from "../../assets/card-img1.png";
+import { useAuthProducts } from "../../context/auth-products-context";
+import ProductCard from "../../components/ProductCard";
 
 export function WishlistItems() {
+  const { state } = useAuthProducts();
   return (
     <>
-      <h2 className="page-title txt-bold mx-auto py-md">My Wishlist (4)</h2>
+      <h2 className="page-title txt-bold mx-auto py-md">
+        My Wishlist ({state["wishlist"].length})
+      </h2>
       <hr />
       <section
         className={`min-h-100 grid ${styles.grid__cols__auto} ${styles.grid__col__1} ${styles.grid__cols__2} gap-2 my-3 px-md`}>
-        <div className={`pos-rel card ${styles.card} mx-auto`}>
-          {false && (
-            <div
-              className={`brief-overlay ${styles.brief__overlay} pos-ab top-left-pos`}>
-              <p
-                className={`overlay-text ${styles.overlay__text} pos-ab txt-black mx-auto txt-center`}>
-                Out&nbsp;Of Stock
-              </p>
-            </div>
-          )}
-          <div className="pos-ab card-badge card-badge-left top-left-pos">
-            10% off
-          </div>
-          <div className={`remove-card-btn pos-ab ${styles.top__right__pos}`}>
-            <i className="far fa-2x fa-times-circle"></i>
-          </div>
-          <div className={`${styles.card__img}`}>
-            <img src={Hat} alt="vertical card" />
-          </div>
-          <div className="card-title flex-col">
-            <p className="txt-md txt-bold mb-sm">MV33 Brimmed Cap</p>
-            <p className="txt-reg txt-semibold">Redball - Hat</p>
-            <div className="flex-row align-center mt-1">
-              <div className="dis-flex flex-wrap align-center">
-                <p className="txt-md txt-bold mr-sm"> ₹3,099/-</p>
-                <s className="strike-color txt-semibold">₹3,400/-</s>
-              </div>
-              <p className="flex-basis-20 txt-sm txt-bold ml-auto rating">
-                <i className="fas fa-sm fa-star"></i>
-                3.6
-              </p>
-            </div>
-          </div>
-          <hr />
-          <div className="card-footer flex-around flex-grow-1">
-            <button className="btn btn-wish btn-md txt-bold txt-reg w-100">
-              <i className="fas fa-shopping-cart"></i>
-              move to cart
-            </button>
-          </div>
-        </div>
-        <div className={`pos-rel card ${styles.card} mx-auto`}>
-          {false && (
-            <div
-              className={`brief-overlay ${styles.brief__overlay} pos-ab top-left-pos`}>
-              <p
-                className={`overlay-text ${styles.overlay__text} pos-ab txt-black mx-auto txt-center`}>
-                Out&nbsp;Of Stock
-              </p>
-            </div>
-          )}
-          <div className="pos-ab card-badge card-badge-left top-left-pos">
-            10% off
-          </div>
-          <div className={`remove-card-btn pos-ab ${styles.top__right__pos}`}>
-            <i className="far fa-2x fa-times-circle"></i>
-          </div>
-          <div className={`${styles.card__img}`}>
-            <img src={Hat} alt="vertical card" />
-          </div>
-          <div className="card-title flex-col">
-            <p className="txt-md txt-bold mb-sm">MV33 Brimmed Cap</p>
-            <p className="txt-reg txt-semibold">Redball - Hat</p>
-            <div className="flex-row align-center mt-1">
-              <div className="dis-flex flex-wrap align-center">
-                <p className="txt-md txt-bold mr-sm"> ₹3,099/-</p>
-                <s className="strike-color txt-semibold">₹3,400/-</s>
-              </div>
-              <p className="flex-basis-20 txt-sm txt-bold ml-auto rating">
-                <i className="fas fa-sm fa-star"></i>
-                3.6
-              </p>
-            </div>
-          </div>
-          <hr />
-          <div className="card-footer flex-around flex-grow-1">
-            <button className="btn btn-wish btn-md txt-bold txt-reg w-100">
-              <i className="fas fa-shopping-cart"></i>
-              move to cart
-            </button>
-          </div>
-        </div>
-        <div className={`pos-rel card ${styles.card} mx-auto`}>
-          {false && (
-            <div
-              className={`brief-overlay ${styles.brief__overlay} pos-ab top-left-pos`}>
-              <p
-                className={`overlay-text ${styles.overlay__text} pos-ab txt-black mx-auto txt-center`}>
-                Out&nbsp;Of Stock
-              </p>
-            </div>
-          )}
-          <div className="pos-ab card-badge card-badge-left top-left-pos">
-            10% off
-          </div>
-          <div className={`remove-card-btn pos-ab ${styles.top__right__pos}`}>
-            <i className="far fa-2x fa-times-circle"></i>
-          </div>
-          <div className={`${styles.card__img}`}>
-            <img src={Hat} alt="vertical card" />
-          </div>
-          <div className="card-title flex-col">
-            <p className="txt-md txt-bold mb-sm">MV33 Brimmed Cap</p>
-            <p className="txt-reg txt-semibold">Redball - Hat</p>
-            <div className="flex-row align-center mt-1">
-              <div className="dis-flex flex-wrap align-center">
-                <p className="txt-md txt-bold mr-sm"> ₹3,099/-</p>
-                <s className="strike-color txt-semibold">₹3,400/-</s>
-              </div>
-              <p className="flex-basis-20 txt-sm txt-bold ml-auto rating">
-                <i className="fas fa-sm fa-star"></i>
-                3.6
-              </p>
-            </div>
-          </div>
-          <hr />
-          <div className="card-footer flex-around flex-grow-1">
-            <button className="btn btn-wish btn-md txt-bold txt-reg w-100">
-              <i className="fas fa-shopping-cart"></i>
-              move to cart
-            </button>
-          </div>
-        </div>
-        <div className={`pos-rel card ${styles.card} mx-auto`}>
-          {false && (
-            <div
-              className={`brief-overlay ${styles.brief__overlay} pos-ab top-left-pos`}>
-              <p
-                className={`overlay-text ${styles.overlay__text} pos-ab txt-black mx-auto txt-center`}>
-                Out&nbsp;Of Stock
-              </p>
-            </div>
-          )}
-          <div className="pos-ab card-badge card-badge-left top-left-pos">
-            10% off
-          </div>
-          <div className={`remove-card-btn pos-ab ${styles.top__right__pos}`}>
-            <i className="far fa-2x fa-times-circle"></i>
-          </div>
-          <div className={`${styles.card__img}`}>
-            <img src={Hat} alt="vertical card" />
-          </div>
-          <div className="card-title flex-col">
-            <p className="txt-md txt-bold mb-sm">MV33 Brimmed Cap</p>
-            <p className="txt-reg txt-semibold">Redball - Hat</p>
-            <div className="flex-row align-center mt-1">
-              <div className="dis-flex flex-wrap align-center">
-                <p className="txt-md txt-bold mr-sm"> ₹3,099/-</p>
-                <s className="strike-color txt-semibold">₹3,400/-</s>
-              </div>
-              <p className="flex-basis-20 txt-sm txt-bold ml-auto rating">
-                <i className="fas fa-sm fa-star"></i>
-                3.6
-              </p>
-            </div>
-          </div>
-          <hr />
-          <div className="card-footer flex-around flex-grow-1">
-            <button className="btn btn-wish btn-md txt-bold txt-reg w-100">
-              <i className="fas fa-shopping-cart"></i>
-              move to cart
-            </button>
-          </div>
-        </div>
+        {state["wishlist"].map((product) => (
+          <ProductCard product={product} />
+        ))}
       </section>
     </>
   );
