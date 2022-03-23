0eb77eec734e615f5f8efd4f64edd07e440482fe
Hamdrive <hamza.husein017@gmail.com>
2022-03-23 21:58:51 +0530

    [feat] Cart page component

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Footer, Navbar } from '../../components'
+import CartItems from './CartItems'
+import styles from "./Cart.module.css"
 
 export function Cart() {
   return (
     <>
       <Navbar />
+      <main className={`${styles.max__width__1000} mx-auto`}>
+        <CartItems />
+      </main>
       <Footer />
     </>
   )
