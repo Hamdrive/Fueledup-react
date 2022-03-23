c5cc9821706eeb670d3c426ef7a356303f23ddf7
Hamdrive <hamza.husein017@gmail.com>
2022-03-24 00:02:39 +0530

    [update] cart page layout

--- src/pages/cart/CartItems.jsx
+++ src/pages/cart/CartItems.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import CartCard from "../../components/CartCard";
 import PriceSummary from "../../components/PriceSummary";
+import { useAuthProducts } from "../../context/auth-products-context";
 import styles from "./Cart.module.css";
 
 export default function CartItems() {
+  const { state } = useAuthProducts();
+  console.log(state)
   return (
-    <section
-      className={`${styles.page__content} grid ${styles.grid__cols__2__1} grid-cols-1 grid-reverse gap-3 pos-rel my-3 px-lg`}>
-      <section className="grid grid-col-1 gap-3">
-        <CartCard />
+    <>
+      <h2 className="page-title txt-bold mx-auto py-md">
+        My Cart ({state["cart"].length})
+      </h2>
+      <hr />
+      <section
+        className={`${styles.page__content} grid ${styles.grid__cols__2__1} grid-cols-1 grid-reverse gap-3 pos-rel my-3 px-lg`}>
+        <section className="grid grid-col-1 gap-3">
+          <CartCard />
+        </section>
+        <PriceSummary />
       </section>
-      <PriceSummary />
-    </section>
+    </>
   );
 }
