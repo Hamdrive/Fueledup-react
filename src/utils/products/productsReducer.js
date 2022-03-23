7a8a17ce70efc2fc40367c0a3ad65d40011269e0
Hamdrive <hamza.husein017@gmail.com>
2022-03-24 00:02:19 +0530

    [feat] add cart switch statements

--- src/utils/products/productsReducer.js
+++ src/utils/products/productsReducer.js
@@ -10,6 +10,14 @@ export function productsReducer(state, action) {
           (item) => item._id !== action.payload._id
         ),
       };
+    case "ADD_TO_CART":
+      return { ...state, cart: [...state.cart, action.payload] };
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state["cart"].filter((item) => item._id !== action.payload._id),
+      };
+
     default:
       return state;
   }
