765e86fae862f87b37269fedfc8f3949baa6de45
Hamdrive <hamza.husein017@gmail.com>
2022-03-24 20:55:28 +0530

    [feat] price summary calculation

--- src/components/PriceSummary.jsx
+++ src/components/PriceSummary.jsx
@@ -1,34 +1,56 @@
-import React from 'react'
+import React from "react";
 
-export default function PriceSummary() {
+export default function PriceSummary({ products }) {
+  const tally = products.reduce(
+    (total, curr) => ({
+      ...total,
+      initPrice: Number(total.initPrice) + Number(curr.price),
+      totalDisc:
+        Number(total.totalDisc) +
+        (Number(curr.price) - Number(curr.productDiscountPrice)),
+      totalPrice: Number(total.totalPrice) + Number(curr.productDiscountPrice),
+    }),
+    {
+      initPrice: 0,
+      totalDisc: 0,
+      totalPrice: 0,
+    }
+  );
+
+  console.log(tally);
+
   return (
     <section className="grid-reverse">
       <h2 className="txt-capitalize">Price details</h2>
       <hr />
       <div className="price-breakup">
         <div className="flex-between my-1">
-          <p>Price (2 items)</p>
-          <p className="txt-semibold">₹9,999/-</p>
+          <p>Price ({[products.length]} items)</p>
+          <p className="txt-semibold">₹ {tally.initPrice}/-</p>
         </div>
         <div className="flex-between my-1">
           <p>Discount</p>
-          <p className="txt-semibold">₹999/-</p>
+          <p className="txt-semibold">₹ {tally.totalDisc}/-</p>
         </div>
         <div className="flex-between my-1">
           <p className="txt-capitalize">Delivery charges</p>
-          <p className="txt-semibold">₹99/-</p>
+          <p className="txt-semibold">
+            ₹ {Math.ceil(tally.totalPrice * 0.05)}/-
+          </p>
         </div>
       </div>
       <hr />
       <div className="price-total flex-between my-1">
         <p className="txt-capitalize txt-bold h4">Total amount</p>
-        <p className="txt-md txt-bold">₹10,998/-</p>
+        <p className="txt-md txt-bold">
+          ₹ {Math.ceil(tally.totalPrice - tally.totalPrice * 0.05)}/-
+        </p>
       </div>
       <hr />
       <div className="total-message my-1">
         <p className="h4 txt-center">
           You will save
-          <strong> ₹999 </strong>
+          <strong> ₹ {tally.totalDisc} </strong>
           on this order!
         </p>
       </div>
--- src/pages/cart/CartItems.jsx
+++ src/pages/cart/CartItems.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import CartCard from "../../components/CartCard";
 import PriceSummary from "../../components/PriceSummary";
 import { useAuthProducts } from "../../context/auth-products-context";
@@ -7,14 +7,8 @@ import styles from "./Cart.module.css";
 
 export default function CartItems() {
   const { state } = useAuthProducts();
-  console.log(state)
+  console.log(state);
 
-  const [tally, setTally] = useState({})
-
-  useEffect(() => {
-    setTally(priceSummaryReducer(state["cart"]))
-  }, [state["cart"]])
-  
   return (
     <>
       <h2 className="page-title txt-bold mx-auto py-md">
@@ -24,9 +18,11 @@ export default function CartItems() {
       <section
         className={`${styles.page__content} grid ${styles.grid__cols__2__1} grid-cols-1 grid-reverse gap-3 pos-rel my-3 px-lg`}>
         <section className="grid grid-col-1 gap-3">
-          {state["cart"].map((product) => (<CartCard key={product._id} product={product} />))}
+          {state["cart"].map((product) => (
+            <CartCard key={product._id} product={product} />
+          ))}
         </section>
-        <PriceSummary tally={tally} />
+        <PriceSummary products={state["cart"]} />
       </section>
     </>
   );
