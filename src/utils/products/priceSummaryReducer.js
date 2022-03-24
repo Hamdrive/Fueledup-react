44ca5d3fea9c21574fe147ef77b507b27954ca5f
Hamdrive <hamza.husein017@gmail.com>
2022-03-24 21:22:29 +0530

    [update] remove reducer

--- src/utils/products/priceSummaryReducer.js
+++
@@ -1,7 +0,0 @@
-export const priceSummaryReducer = ([...items]) =>
-  items.reduce((priceTally, currItem) => {}, {
-    initPrice: 0,
-    totalDisc: 0,
-    totalPrice: 0,
-    totalItems: 0,
-  });
