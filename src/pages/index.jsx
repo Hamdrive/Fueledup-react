2dc148e548eab99f1e2731fc489fa2a5d830084b
Hamdrive <hamza.husein017@gmail.com>
2022-04-02 14:41:36 +0530

    [feat] setup 404 page route

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,3 +4,4 @@ export { Cart } from "./cart/Cart";
 export { Wishlist } from "./wishlist/Wishlist";
 export { Signup } from "./signup/Signup";
 export { Login } from "./login/Login";
+export { PageNotFound } from "./PageNotFound/PageNotFound";
