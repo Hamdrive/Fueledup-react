01429f6ba55f452b44658a9c1d4c35a78d93d337
Hamdrive <hamza.husein017@gmail.com>
2022-04-03 06:53:32 +0530

    [feat] products splashscreen

--- src/pages/products/ProductItems.jsx
+++ src/pages/products/ProductItems.jsx
@@ -2,19 +2,33 @@ import styles from "./Products.module.css";
 import React from "react";
 import { useProducts } from "../../context/product-context";
 import ProductCard from "../../components/ProductCard";
+import ProductsNotFound from "../../assets/ProductsNotFound.png";
 
 export default function ProductItems() {
   const { finalProducts } = useProducts();
 
   return (
     <section
-      className={`min-h-95 grid ${styles.grid__cols__auto} ${styles.grid__col__1} ${styles.grid__cols__2} gap-2 my-3 px-md`}>
-      {finalProducts.length === 0 && (
-        <div className="h1">No Products Available :(</div>
+      className={`min-h-95 grid ${styles.grid__cols__auto} gap-2 my-3 px-md`}>
+      {finalProducts.length > 0 ? (
+        finalProducts.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))
+      ) : (
+        <div className={`${finalProducts.length === 0 && "span-auto"}`}>
+          <div className="h-100 flex-column flex-center">
+            <img
+              className={`${styles.empty__products}`}
+              src={ProductsNotFound}
+              alt="No products found"
+            />
+            <div className="flex-column align-center txt-center">
+              <h3>This place is devoid of any life!</h3>
+              <p>We couldn't find any products, try changing your filters.</p>
+            </div>
+          </div>
+        </div>
       )}
-      {finalProducts.map((product) => (
-        <ProductCard key={product._id} product={product} />
-      ))}
     </section>
   );
 }
