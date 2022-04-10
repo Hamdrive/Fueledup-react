828cfededf59b84f229daed47ae19bff9634832d
Hamdrive <hamza.husein017@gmail.com>
2022-04-10 12:45:01 +0530

    [feat] setup routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import {Routes, Route} from "react-router-dom"
-import {Home, Products, Cart, Wishlist, Login, Signup, PageNotFound} from "./pages"
+import { Routes, Route } from "react-router-dom";
+import { MockAPI } from "./components";
+import {
+  Home,
+  Products,
+  Cart,
+  Wishlist,
+  Login,
+  Signup,
+  PageNotFound,
+} from "./pages";
+import { RequireAuth } from "./router/RequireAuth";
 
 export default function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/products" element={<Products />}/>
-        <Route path="/cart" element={<Cart />}/>
-        <Route path="/wishlist" element={<Wishlist />}/>
-        <Route path="/signup" element={<Signup />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<PageNotFound />} />
+      <Route
+        path="/cart"
+        element={
+          <RequireAuth>
+            <Cart />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/wishlist"
+        element={
+          <RequireAuth>
+            <Wishlist />
+          </RequireAuth>
+        }
+      />
+      <Route path="/mockman" element={<MockAPI />} />
+    </Routes>
   );
 }
