8f6f40643cee039dd117899bf2cb38dab4b7ec47
Hamdrive <hamza.husein017@gmail.com>
2022-04-10 13:00:58 +0530

    [chore] update className JSX

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,7 +5,7 @@ export function ScrollToTop() {
     <button
       onScroll={onScroll}
       className={`btn btn-sec btn-fab bottom-right-10`}>
-      <i class="fas fa-angle-up"></i>
+      <i className="fas fa-angle-up"></i>
     </button>
   );
 }
