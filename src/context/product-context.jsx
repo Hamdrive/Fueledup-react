d35c02c6d999a05cea89197e2d98849d506be8e8
Hamdrive <hamza.husein017@gmail.com>
2022-04-10 13:00:34 +0530

    [feat] new product context

--- src/context/product-context.jsx
+++ src/context/product-context.jsx
@@ -1,10 +1,9 @@
-import axios from "axios";
 import {
   createContext,
   useContext,
-  useState,
   useReducer,
   useEffect,
+  useState,
 } from "react";
 import {
   fastDelivery,
@@ -14,30 +13,31 @@ import {
   userBrand,
   userCategory,
   userRating,
-} from "../utils/filters/filterFunctions";
-import { applyFilters } from "../utils/filters/applyFilters";
-import { filterSpecification } from "../utils/filters/filterSpecification";
-import { getFinalProducts } from "../utils/filters/getFinalProducts";
+  getFinalProducts,
+  initialDataState,
+  productReducer,
+} from "../utils";
+import {
+  performAddToCart,
+  performAddToWishlist,
+  performGetCart,
+  performGetCategories,
+  performGetProducts,
+  performGetWishlist,
+  performRemoveFromCart,
+  performRemoveFromWishlist,
+  performUpdateCartQuantity,
+} from "../utils/server-routes";
+import { useAuth } from "./auth-context";
 
-const ProductContext = createContext(filterSpecification);
+const ProductContext = createContext(initialDataState);
 
-const useProducts = () => useContext(ProductContext);
+const useProduct = () => useContext(ProductContext);
 
 const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
-  const [state, dispatch] = useReducer(applyFilters, filterSpecification);
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await axios.get("/api/categories");
-        setCategories(res.data.categories);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, []);
+  const [loader, setLoader] = useState(false);
+  const [state, dispatch] = useReducer(productReducer, initialDataState);
+  const { userToken } = useAuth();
 
   const finalProducts = getFinalProducts(
     sortByPrice,
@@ -47,13 +47,106 @@ const ProductProvider = ({ children }) => {
     userCategory,
     userBrand,
     priceRange
-  )(state, [...products]);
+  )(state.filters, [...state.products]);
+
+  const addToCart = async (product) => {
+    try {
+      const res = await performAddToCart(product, userToken);
+      console.log(res);
+      dispatch({ type: "ADD_TO_CART", payload: res.data.cart });
+      console.log(state);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getCart = async () => {
+    try {
+      const res = await performGetCart(userToken);
+      console.log(res);
+      dispatch({ type: "GET_CART", payload: res.data.cart });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateCartQuantity = async (id, type) => {
+    try {
+      const res = await performUpdateCartQuantity(id, type, userToken);
+      console.log(res);
+      dispatch({
+        type: "UPDATE_CART",
+        payload: res.data.cart,
+      });
+    } catch (error) {}
+  };
+
+  const removeFromCart = async (id) => {
+    try {
+      const res = await performRemoveFromCart(id, userToken);
+      console.log(res);
+      dispatch({
+        type: "REMOVE_FROM_CART",
+        payload: res.data.cart,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addToWishlist = async (product) => {
+    try {
+      const res = await performAddToWishlist(product, userToken);
+      dispatch({
+        type: "ADD_TO_WISHLIST",
+        payload: res.data.wishlist,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getWishlist = async (product) => {
+    try {
+      const res = await performGetWishlist(product, userToken);
+      dispatch({ type: "GET_WISHLIST", payload: res.data.wishlist });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeFromWishlist = async (id) => {
+    try {
+      const res = await performRemoveFromWishlist(id, userToken);
+      dispatch({
+        type: "REMOVE_FROM_WISHLIST",
+        payload: res.data.wishlist,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     (async () => {
       try {
-        const res = await axios.get("/api/products");
-        setProducts(res.data.products);
+        const res = await performGetCategories();
+        if (res.status === 200 || res.status === 201) {
+          dispatch({ type: "SET_CATEGORIES", payload: res.data.categories });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await performGetProducts();
+        if (res.status === 200 || res.status === 201) {
+          dispatch({ type: "SET_PRODUCTS", payload: res.data.products });
+        }
       } catch (error) {
         console.log(error);
       }
@@ -61,10 +154,22 @@ const ProductProvider = ({ children }) => {
   }, []);
 
   return (
-    <ProductContext.Provider value={{ categories, finalProducts, state, dispatch }}>
+    <ProductContext.Provider
+      value={{
+        state,
+        dispatch,
+        finalProducts,
+        addToWishlist,
+        getWishlist,
+        removeFromWishlist,
+        addToCart,
+        getCart,
+        updateCartQuantity,
+        removeFromCart,
+      }}>
       {children}
     </ProductContext.Provider>
   );
 };
 
-export { ProductProvider, useProducts };
+export { ProductProvider, useProduct };
