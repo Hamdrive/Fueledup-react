fb557c4eafd5685fd875da740ceb05101628d298
Hamdrive <hamza.husein017@gmail.com>
2022-04-12 07:03:10 +0530

    [update] general styles

--- src/pages/singleProduct/SingleProduct.jsx
+++ src/pages/singleProduct/SingleProduct.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { SingleProductCard } from "../../components";
+import { Footer, Navbar, SingleProductCard } from "../../components";
 import { useProduct } from "../../context/product-context";
 
 export function SingleProduct() {
   const { productId } = useParams();
   const { finalProducts } = useProduct();
 
-  const product = finalProducts.filter((item) => item._id === productId)
+  const product = finalProducts.filter((item) => item._id === productId);
 
-  return <div><SingleProductCard product={product} /></div>;
+  return (
+    <div>
+      <Navbar />
+      <main className="pos-rel min-h-95 flex-column align-center">
+        <SingleProductCard product={product[0]} />
+      </main>
+      <Footer />
+    </div>
+  );
 }
