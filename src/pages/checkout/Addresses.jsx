262b79083a55939626e27aab4011dc01136e3764
Hamdrive <hamza.husein017@gmail.com>
2022-04-15 12:18:09 +0530

    [update] dummy addresses radio styles

--- src/pages/checkout/Addresses.jsx
+++ src/pages/checkout/Addresses.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Checkout.module.css";
 
 export function Addresses() {
   return (
@@ -6,16 +7,31 @@ export function Addresses() {
       <h3 className="txt-capitalize txt-left">Address Book</h3>
       <div className="my-2">
         <ul>
-          <li className="flex-row">
-            <input className="w-10" type="radio" name="address" id="address" />
+          <li className={styles.address__block}>
+            <label>
+              <input
+                className="mx-2 large-radio"
+                type="radio"
+                name="address"
+                id="address"
+                checked
+              />
+            </label>
             <div className="w-100">
               <p className="h3">Carlos Sainz Jr.</p>
               <p>Viale di Vedano, 5, 20900 Monza MB, Italy</p>
               <p>123-456-789</p>
             </div>
           </li>
-          <li className="flex-row gap-1">
-            <input className="w-30 radiobutton" type="radio" name="address" id="address" />
+          <li className={styles.address__block}>
+            <label>
+              <input
+                className="mx-2 large-radio"
+                type="radio"
+                name="address"
+                id="address"
+              />
+            </label>
             <div className="w-100">
               <p className="h3">Lewis Hamilton</p>
               <p>Towcester NN12 8TN, United Kingdom</p>
