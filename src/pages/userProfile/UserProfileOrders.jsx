ad65273b021caf6e85780f3dbae44ef7c3191700
Hamdrive <hamza.husein017@gmail.com>
2022-04-17 15:36:01 +0530

    [update] handle empty orders text

--- src/pages/userProfile/UserProfileOrders.jsx
+++ src/pages/userProfile/UserProfileOrders.jsx
@@ -14,24 +14,30 @@ export function UserProfileOrders({}) {
       <div className="txt-wrap">
         <h3 className="py-sm">Order Summary</h3>
         <section className={styles.products__order}>
-          {orders.map((item) => (
-            <div key={item._id} className={styles.product}>
-              <section>
-                <img src={item.productImage} alt="product image" />
-              </section>
-              <section className="flex-column">
-                <p className="flex-grow-1">{item.title}</p>
-                <p className="flex-grow-1">
-                  {item.team} - {item.categoryName}
-                </p>
-                <p className="flex-grow-1">Quantity: {item.qty}</p>
-                <p className="flex-grow-1">
-                  Total Price: ₹{" "}
-                  {Number(item.productDiscountPrice) * Number(item.qty)}/-
-                </p>
-              </section>
+          {orders?.length > 0 ? (
+            orders.map((item) => (
+              <div key={item._id} className={styles.product}>
+                <section>
+                  <img src={item.productImage} alt="product image" />
+                </section>
+                <section className="flex-column">
+                  <p className="flex-grow-1">{item.title}</p>
+                  <p className="flex-grow-1">
+                    {item.team} - {item.categoryName}
+                  </p>
+                  <p className="flex-grow-1">Quantity: {item.qty}</p>
+                  <p className="flex-grow-1">
+                    Total Price: ₹{" "}
+                    {Number(item.productDiscountPrice) * Number(item.qty)}/-
+                  </p>
+                </section>
+              </div>
+            ))
+          ) : (
+            <div className="flex-center">
+              <p className="h3">No orders yet...</p>
             </div>
-          ))}
+          )}
         </section>
       </div>
     </>
